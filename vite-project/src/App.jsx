2cd1dabc01911d22ae6925bc8efc1218d224eff0
pruthal2309 <pruthalhirpara09@gmail.com>
2026-08-23 17:18:58 +0530

    Basic Routing in React

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+
+function Home(){
+  return <h1 className="flex gap-4 px-40 py-10">This is Home Page</h1>
+}
+
+
+function About(){
+  return <h1>This is about Page</h1>
+}
+
+function Contact(){
+  return <h1>This is Contact Page</h1>
+}
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <div>
+        <nav className="flex gap-4 p-4">
+          <Link to="/"> Home </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/contactUs"> Contact </Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contactUs" element={<Contact />}></Route>
+          
+        </Routes>
+    </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
