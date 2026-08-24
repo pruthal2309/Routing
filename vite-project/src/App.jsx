f7366bb3848ceceb14cf441c96e907d12437f4e3
pruthal2309 <pruthalhirpara09@gmail.com>
2026-08-24 22:47:44 +0530

    Dynamic Routing

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Courses from './components/Courses'
+import NotFound from './components/NotFound'
 
 
 const App = () => {
@@ -13,12 +15,16 @@ const App = () => {
           <Link to="/"> Home </Link>
           <Link to="/about"> About </Link>
           <Link to="/contactUs"> Contact </Link>
+          <Link to="/courses"> Courses</Link>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/contactUs" element={<Contact />}></Route>
+          <Route path="/courses" element={<Courses />}></Route>
+          <Route path="/courses/:id" element={<Courses />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
     </div>
     </BrowserRouter>
